527878692497f2c19152d380eb507cb5a441698f
Takashi-1027 <taka19921027@gmail.com>
2021-02-18 14:18:46 +0000

    [STEP1] finished capter2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   protected
   #strong parametersを設定し、usernameを許可
   def configure_permitted_parameters
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email]) #:emailのデータ操作を許可されるように
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email]) #:emailのデータ操作を許可されるように
+    #devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :password, :password_confirmation) }
+    #devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :password, :password_confirmation) }
   end
 
 end
