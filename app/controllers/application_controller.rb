fcc716c694e36f90788cabee824ee1c228d34cfc
Takashi-1027 <taka19921027@gmail.com>
2021-02-18 11:59:08 +0000

    [STEP1]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  #strong parametersを設定し、usernameを許可
+  def configure_permitted_parameters
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email]) #:emailのデータ操作を許可されるように
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :password, :password_confirmation) }
+  end
+
 end
